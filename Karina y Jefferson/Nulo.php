742192b11a5293dee7bbce303f20fe57a9bff7d7
July Sierra Sierra <jdayanse@misena.edu.co>
2016-04-28 10:23:16 -0500

    28/04/2016

---
+++ Karina y Jefferson/Nulo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+$var1 = "July";
+echo "$var1";
+$var = NULL;       
+?>
+
+</body>
+</html>
---
+++ Karina y Jefferson/Objetos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<pre>
+<body>
+
+<?php 
+
+class foo
+{
+    function hacer_algo()
+    {
+        echo "Haciendo algo."; 
+    }
+}
+
+$bar = new foo;
+$bar->hacer_algo();
+
+
+?>
+
+</pre>
+</body>
+</html>
