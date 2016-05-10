ef6fde2ac3bb0cccbd85794133ff3848acf20321
July Sierra Sierra <jdayanse@misena.edu.co>
2016-05-10 07:30:51 -0500

    Sonia

--- Profe Diego Operadores/OperadoresArrays.php
+++ Profe Diego Operadores/OperadoresArrays.php
@@ -5,22 +5,26 @@
 </head>
 <body>
 <pre>
+
 <?php
-$a = array("a" => "apple", "b" => "banana");
-$b = array("a" => "pear", "b" => "strawberry", "c" => "cherry");
 
-$c = $a + $b; // Unión de $a y $b
-echo "Unión de \$a y \$b: \n";
-var_dump($c);
+	$a = array("a" => "apple", "b" => "banana");
+	$b = array("a" => "pear", "b" => "strawberry", "c" => "cherry");
 
-$c = $b + $a; // Unión de $b y $a
-echo "Unión de \$b y \$a: \n";
-var_dump($c);
+	$c = $a + $b; // Unión de $a y $b
+	echo "Unión de \$a y \$b: \n";
+	var_dump($c);
 
-$a += $b; // Unión de $a += $b es $a y $b
-echo "Unión de \$a += \$b: \n";
-var_dump($a);
+	$c = $b + $a; // Unión de $b y $a
+	echo "Unión de \$b y \$a: \n";
+	var_dump($c);
+
+	$a += $b; // Unión de $a += $b es $a y $b
+	echo "Unión de \$a += \$b: \n";
+	var_dump($a);
+
 ?>
+
 </pre>
 </body>
 </html>
