0fc25f10866baea1a1bae6ecc05795704d1e4bd8
July Sierra Sierra <jdayanse@misena.edu.co>
2016-05-10 06:25:45 -0500

    10/05/2016

--- Mayra y Claudia/prueba.php
+++ Mayra y Claudia/prueba.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 
 $foo = $bar; //notice : $bar uninitialized
 
-$bar['foo'] = 'hello'; // no notice, although $bar itself has never been initialized (with "$bar = array()" for example)
+$bar['foo'] = 'hello'; // no notice, although $bar itself has never been initialized (for example with "$bar = array()" )
 
 $bar = array('foobar' => 'barfoo');
 $foo = $bar['foobar'] // ok
--- Mayra y Claudia/trigger_error.php
+++ Mayra y Claudia/trigger_error.php
@@ -1,5 +1,8 @@
 <?php
-if ($divisor == 0) {
-    trigger_error("No se puede dividir por cero", E_USER_ERROR);
-}
+
+	if ($divisor == 0) {
+    	
+    	trigger_error("No se puede dividir por cero", E_USER_ERROR);
+	}
+
 ?>
